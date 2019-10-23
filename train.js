f82de5c1467f3079ac134633e6f4ffffd3266508
ctslone <ctslone27@gmail.com>
2019-10-23 15:55:00 -0400

    adding functionality with Moment JS to show next train time and minutes till, depending on the users input of frequency

--- train.js
+++ train.js
@@ -23,16 +23,15 @@ $(document).ready(function () {
     // creating an on click event listener for the choo choo button
     $("#choo-choo-btn").on("click", function (form) {
         form.preventDefault();
-
         // re-declaring the variables to the user input
         trainName = $("#train-name-input").val().trim();
-            console.log(trainName);
+            // console.log(trainName);
         destination = $("#destination-input").val().trim();
-            console.log(destination);
+            // console.log(destination);
         frequency = $("#frequency-input").val().trim();
-            console.log(frequency);
+            // console.log(frequency);
         firstTime = $("#first-time-input").val().trim();
-            console.log(firstTime);
+            // console.log(firstTime);
         // console.log("These are the variables:" + trainName, destination, frequency, nextArrival, minsAway)
 
         // creating new child for the variables in firebase
@@ -43,6 +42,7 @@ $(document).ready(function () {
             firstTime: firstTime,
             dateAdded: firebase.database.ServerValue.TIMESTAMP
         });
+    })
 
         // listener for adding a child to the DB
         database.ref().on("child_added", function(snapshot) {
@@ -56,36 +56,42 @@ $(document).ready(function () {
             console.log("snapshot: " + sv.firstTime);
             console.log("snapshot: " + sv.dateAdded);
 
-            var nameTd = $("<td>").text(sv.trainName);
-            var destinationTd =$("<td>").text(sv.destination);
-            var frequencyTd = $("<td>").text(sv.frequency);
-            var nextArrivalTd =$("<td>").text(moment(firstTime).format("HH:mm"));
-            var minutesAwayTd =$("<td>");
-
             var currentTime = moment();
+            var firstTrain = moment(sv.firstTime, "HH:mm").subtract(1, "years");
+                console.log("First Train: "+firstTrain)
+            var diffTime = moment().diff(moment(firstTrain), "minutes");
+                console.log("Difference in time is: " + diffTime);
+            var remainTime = diffTime % sv.frequency;
+                console.log("Remainder minutes: "+remainTime);
+            var timeTillNext = sv.frequency - remainTime;
+                console.log("Time till next train: "+timeTillNext);
+            var nextArrival = moment().add(timeTillNext, "minutes")
 
-            // logging the times
-            console.log("Next time is " + nextArrivalTd)
-
+            var nameTd = $("<td>").text(sv.trainName);
+            var destinationTd = $("<td>").text(sv.destination);
+            var frequencyTd = $("<td>").text(sv.frequency);
+            var nextArrivalTd = $("<td>").text(timeTillNext);
+            var minutesAwayTd = $("<td>").text(moment(nextArrival).format("h:mm a"));
 
             var tR = $("<tr>");
-            tR.append(nameTd, destinationTd, frequencyTd, nextArrivalTd, minutesAwayTd)
-            $("#train-table").append(tR);
+            tR.append(nameTd, destinationTd, frequencyTd, minutesAwayTd, nextArrivalTd)
+            $("#train-table").prepend(tR);
+            // console.log("appended")
             
-            // want to make table scroll after a certain number of entries: read this https://stackoverflow.com/questions/2117320/set-maximum-displayed-rows-count-for-html-table
+            // playing a sweet train sound when the user adds a train!
+            trainHorn();
         })
 
-        // playing a sweet train sound when the user adds a train!
-        trainHorn();
-})
+        
+
 
 
 // FUNCTIONS
 
-function trainHorn() {
-    var trainHorn = document.createElement("audio");
-    trainHorn.src = "media/horn.mp3"
-    trainHorn.play();
+function trainHorn(horn) {
+    var horn = document.createElement("audio");
+    horn.src = "media/horn.mp3"
+    horn.play();
 }
 
 
